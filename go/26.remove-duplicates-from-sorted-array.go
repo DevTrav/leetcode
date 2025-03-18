7b806c63a9e81b58339e94d863daa59798e14874
Trav La Ville <18622201+DevTrav@users.noreply.github.com>
2025-03-17 19:37:57 -0500

    Updated solution to two-pointer method for faster run time

--- go/26.remove-duplicates-from-sorted-array.go
+++ go/26.remove-duplicates-from-sorted-array.go
@@ -5,14 +5,32 @@
  */
 
 // @lc code=start
+//for loop solution
+// func removeDuplicates(nums []int) int {
+// 	for i := 1; i < len(nums); i++ {
+// 		if nums[i] == nums[i-1] {
+// 			nums = append(nums[:i], nums[i+1:]...)
+// 			i--
+// 		}
+// 	}
+// 	return len(nums)
+// }
+
+// two pointer solution
 func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	k := 1 //pointer for unique elements
+
 	for i := 1; i < len(nums); i++ {
-		if nums[i] == nums[i-1] {
-			nums = append(nums[:i], nums[i+1:]...)
-			i--
+		if nums[i] != nums[i-1] {
+			nums[k] = nums[i]
+			k++
 		}
 	}
-	return len(nums)
+	return k
 }
 
 // @lc code=end
