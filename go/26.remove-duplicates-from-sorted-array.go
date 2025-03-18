591761b35470a7ed96c3a655b6aa8482c85eb4ef
Trav La Ville <18622201+DevTrav@users.noreply.github.com>
2025-03-17 19:19:02 -0500

    remove duplicates from sorted array for loop solution

---
+++ go/26.remove-duplicates-from-sorted-array.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=26 lang=golang
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			nums = append(nums[:i], nums[i+1:]...)
+			i--
+		}
+	}
+	return len(nums)
+}
+
+// @lc code=end
+
